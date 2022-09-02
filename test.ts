7c7ba0ebecaff6698bb7fe11bad15eac37faee81
Bill Siever <bsiever@gmail.com>
2022-09-01 19:43:08 -0500

    Added test.

--- test.ts
+++ test.ts
@@ -5,7 +5,29 @@
 //proportionalFont.showNumber(1234, 100)
 //proportionalFont.showString("test okay")
 //proportionalFont.showString("test okay", 100)
-proportionalFont.showSpace(2)
-proportionalFont.showSpace(2, 100)
+// proportionalFont.showSpace(2)
+// proportionalFont.showSpace(2, 100)
 //basic.showNumber(proportionalFont.getWidthOfNumber(1234))
 //basic.showNumber(proportionalFont.getWidthOfString("test okay"))
+input.onButtonPressed(Button.A, function () {
+    proportionalFont.setDirection(proportionalFont.DIRECTION.UpsideDown)
+})
+input.onButtonPressed(Button.AB, function () {
+    proportionalFont.setDirection(proportionalFont.DIRECTION.LogoOnRight)
+})
+input.onButtonPressed(Button.B, function () {
+    proportionalFont.setDirection(proportionalFont.DIRECTION.LogoOnLeft)
+})
+input.onGesture(Gesture.Shake, function () {
+    proportionalFont.setDirection(proportionalFont.DIRECTION.Normal)
+})
+let count = 0
+proportionalFont.setDirection(proportionalFont.DIRECTION.Normal)
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    proportionalFont.showSpace(4, 150)
+    proportionalFont.showString("Hello. ", 150)
+    count += 1
+    proportionalFont.showNumber(count, 150)
+    proportionalFont.showSpace(4, 150)
+})
